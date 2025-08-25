fd77a0647ef0511589ed0cfa293223929675ffa0
Jay <mercinesjohnjay@gmail.com>
2025-08-25 20:30:57 +0800

    remove unused codes

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,15 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [dailyCalories, setDailyCalories] = useState(1650)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-200">Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="text-xl bg-gray-200 h-screen">
+        <button onClick={() => setDailyCalories((dailyCalories) => dailyCalories + 1)}>
+          Calories is <p className="inline font-bold">{dailyCalories}</p>
         </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
       </div>
     </>
   )

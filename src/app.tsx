61f17a178e495a35e5a8350f7e6520faf356e532
Jay <mercinesjohnjay@gmail.com>
2025-08-25 19:57:22 +0800

    setup preact + ts

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}

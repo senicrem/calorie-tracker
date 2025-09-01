db07a9d7586f11b6042aa7cf47debcfe9986ffec
Jay <mercinesjohnjay@gmail.com>
2025-09-01 15:52:22 +0800

    add multiple items delete func

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import './app.css'
 import { h } from 'preact'
 import { useState, useEffect } from 'preact/hooks'
 import type { FormEvent } from 'preact/compat'
-import list from "./data/meallist.json"
+import meallist from "./data/meallist.json"
 
 export function App() {
   const [dailyCalories, setDailyCalories] = useState(1500)
 	const [isOpenModal, setIsOpenModal ] = useState<boolean>(false)
+	const [foods, setFoods] = useState(meallist)
+	const [checkedMeal, setCheckedMeal] = useState<string[]>([])
 	const [formData, setFormData ] = useState<MealForm>({
 		id: "",
 		title: "",
@@ -15,15 +17,16 @@ export function App() {
 		type: ""
 	})
 
-	const foods = Object.groupBy(list, (l) => l.type);
+
   const caloriesData = [
     { type: "consumed", calories: 100 },
     { type: "remaining", calories: 1400 },
   ]
-console.log(foods);
+
   useEffect(() => {
     console.log("load database here!")
-  }, []);
+    console.log("checkedMeal", checkedMeal)
+  }, [checkedMeal]);
 
   const caloriesPerCategory = (list:MealForm[] | undefined) => {
 		if (list === undefined) return 0;
@@ -76,13 +79,42 @@ console.log(foods);
     closeModal()
 	}
 
-	const onClickMealList = (id: string) => {}
+	const handleCheckBox = (e: h.JSX.TargetedEvent<HTMLInputElement, Event>) => {
+		const { checked, value: checkValue } = e.currentTarget;
+
+		if (checked) {
+			setCheckedMeal([...checkedMeal, checkValue])
+		}
+
+		if (!checked) {
+			const idx = checkedMeal.findIndex(meal => meal === checkValue);
+			checkedMeal.filter((_, i) => i != idx)
+			setCheckedMeal([
+				...checkedMeal.filter((_, i) => i != idx)
+			])
+		}
+	}
+
+	const deleteMeals = () => {
+		const fud = foods.filter(f => {
+			if (checkedMeal.includes(f.id)) return;
+			return f
+		})
+
+		console.log("fud", fud)
+		setFoods(fud)
+	}
 
   return (
     <>
       <div className="flex justify-center bg-gray-200">
         <div className="w-[700px] h-screen bg-white p-2">
-          <div className="grid grid-cols-2 gap-2">
+					<div className="py-2 w-full bg-gray-300">
+            <div className="text-center">Today</div>
+            <p></p>
+          </div>
+
+          <div className="mt-1 grid grid-cols-2 gap-1">
             {caloriesData.map((x) => (
               <div className="p-1 bg-gray-200 capitalize text-center">
                 <p className="text-2xl font-semibold">{ x.calories }</p>
@@ -91,35 +123,27 @@ console.log(foods);
             ))}
           </div>
 
-          <div className="mt-2 py-2 w-full bg-gray-300">
-            <div className="text-center">Today</div>
-            <p></p>
+					<div className="mt-1 w-full grid grid-cols-2">
+            <button className="bg-green-100 p-1 cursor-pointer" onClick={openModal}>Add Meal</button>
+            <button className="bg-red-100 p-1 cursor-pointer" onClick={deleteMeals}>
+							Delete <span className={` ${checkedMeal.length ? 'inline-block': 'hidden'}`}>({checkedMeal.length})</span>
+						</button>
           </div>
 
           <div className="w-full mt-2">
-          {Object.entries(foods).map(([mealType, mealList]) => (
-              <div>
-                <div className="bg-gray-200 p-2 uppercase flex justify-between items-center">
-                <div className="flex gap-2">
-                  <p className="bg-black text-white flex justify-center items-center p-1 hover:scale-110 transition-transform cursor-pointer" onClick={openModal}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                    </svg>
-                  </p>
-                  <p>{ mealType }</p>
-                </div>
-                  <p>{ caloriesPerCategory(mealList) }</p>
-                </div>
+          {foods.map((meal) => (
+              <div key={meal.id}>
                 <div>
-                  {mealList?.map((meal) => (
 										<div className="group flex justify-between items-center border-b-1 transition-all border-b-gray-200 hover:bg-gray-50">
-											<div className="px-2">
-												<p>{ meal.title }</p>
-												<p className="text-sm">{ meal.description }</p>
+											<div className="px-2 flex items-center gap-5">
+												<input type="checkbox" className="h-5 w-5 accent-gray-600" value={meal.id} name="list" onClick={handleCheckBox} />
+												<div>
+													<p>{ meal.title }</p>
+													<p className="text-sm">{ meal.description }</p>
+												</div>
 											</div>
 											<p className="p-1 font-semibold">{ meal.calories}</p>
 										</div>
-                  ))}
                 </div>
               </div>
             ))}

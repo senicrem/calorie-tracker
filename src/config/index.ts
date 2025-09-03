fefb096fb49905da8b6dbeb4f98c59258c3f6ae4
Jay <mercinesjohnjay@gmail.com>
2025-09-03 13:01:11 +0800

    add notification

--- src/config/index.ts
+++
@@ -1,3 +0,0 @@
-export const MEAL_TYPE_BREAKFAST = "breakfast"
-export const MEAL_TYPE_LUNCH = "lunch"
-export const MEAL_TYPE_DINNER = "dinner"
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,6 @@
-import { MEAL_TYPE_BREAKFAST, MEAL_TYPE_LUNCH, MEAL_TYPE_DINNER } from '@/config'
-
-declare global {
-  interface MealForm {
-    id: string;
-    title: string;
-    description: string;
-    calories: number;
-    type: MEAL_TYPE_BREAKFAST | MEAL_TYPE_LUNCH | MEAL_TYPE_DINNER | null
-  }
-  
-}
+export interface MealForm {
+  id: string;
+  title: string;
+  description: string;
+  calories: number;
+} 
